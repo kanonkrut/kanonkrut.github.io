9dbdbfa1760be64d71cd6a3f4aa77c8039d132bf
kanonkrut <serik.lindh@gmail.com>
2021-04-12 14:52:37 +0100

    beräkningar

--- calculator.js
+++ calculator.js
@@ -29,22 +29,28 @@ function getCurrentLeverageAmountValue() {
     return getInputValueAsNumber("currentLeverageAmountTbox");
 }
 
+function calculateTotalAmountWithSuperRanta(){
+    var currentSuperRantaAmount = getCurrentSuperRanta();
+
+    return currentSuperRantaAmount * 2;
+}
+
 function calculateLeverageAmountForTargetLeverage() {
-    var currentTotalAmount = getCurrentTotalAmount();
+    var currentAmountWithSuperRanta = calculateTotalAmountWithSuperRanta();
     var targetLeverage = getTargetLeverage();
     var targetLeverageFactor = targetLeverage / 100;
 
     var leverageAmount = 0;
-    var lastLeveragableAmount = currentTotalAmount;
+    var lastLeveragableAmount = currentAmountWithSuperRanta;
 
     var loopCounter = 0;
-    while(lastLeveragableAmount > 1) {
+    while (lastLeveragableAmount > 1) {
         var newLeverage = lastLeveragableAmount * targetLeverageFactor;
         leverageAmount += newLeverage;
         lastLeveragableAmount = newLeverage;
         loopCounter++;
 
-        if(loopCounter > 1000){
+        if (loopCounter > 1000) {
             console.log("Loop counter max hit, abort loop");
             break;
         }
@@ -57,7 +63,7 @@ function printToLabel(id, value, text) {
     document.getElementById(id).innerHTML = text + ": " + value;
 }
 
-function calculateLeverage(){
+function calculateLeverage() {
     var targetLeverageAmount = calculateLeverageAmountForTargetLeverage();
     printToLabel("targetLeverageAmountLabel", targetLeverageAmount, "Total belåning SEK");
 
@@ -68,6 +74,17 @@ function calculateLeverage(){
     var currentTotalAmount = getCurrentTotalAmount();
     var newTotalAmount = amountToInvest + currentTotalAmount;
     printToLabel("newTotalAmountLabel", newTotalAmount, "Nytt totalbelopp");
+
+    var currentTotalAmountSuperRanta = calculateTotalAmountWithSuperRanta();
+    printToLabel("currentTotalAmountSuperRantaLabel", currentTotalAmountSuperRanta, "Nuvarande totalbelopp med superränta");
+    
+    var newTotalAmountSuperRanta = currentTotalAmountSuperRanta + amountToInvest;
+    var newTotalAmountSecondBestInterest  = newTotalAmountSuperRanta / 2;
+    var newTotalAmountBestInterest  = newTotalAmountSuperRanta / 5;
+
+    printToLabel("bestInterestLevelLabel", newTotalAmountBestInterest, "Nytt tak 0% ränta");
+    printToLabel("secondBestLevelLabel", newTotalAmountSecondBestInterest, "Nytt tak 1,24% ränta");
+    printToLabel("superRantaLevelLabel", newTotalAmountSuperRanta, "Nytt tak 2,21% ränta");
 
     return false;
 }
